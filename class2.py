962e5d1ab82351ecf2f4d2b0d628d61ed179d156
akashsatdeve <akashsatdeve8@gmail.com>
2023-10-17 13:12:51 +0530

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,9 @@
+class Hello:
+    def __init__(self,name,rollno):
+        self.name=name
+        self.rollno=rollno
+obj1=Hello("Mukesh",1122)
+obj2=Hello("Ritesh",3322)
+print(obj1.name) 
+print(obj2.rollno)       
+        
